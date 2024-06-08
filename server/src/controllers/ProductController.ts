a1931eb0b565d7aef00a85d886d1906ce8d4db1f
nrc2 <nathyre2002@gmail.com>
2024-06-08 17:54:45 -0300

    feat(ProductController): implement update method

--- server/src/controllers/ProductController.ts
+++ server/src/controllers/ProductController.ts
@@ -4,6 +4,8 @@ import { Product, UpdateProduct } from '../DTOs';
 import { collection, addDoc, getDocs, doc, updateDoc, query, where } from 'firebase/firestore';
 
 class ProductController {
+
+    //CREATE METHOD
     async create(req: Request, res: Response, next: NextFunction) {
         try {
             const productData = Product.parse(req.body);
@@ -18,6 +20,30 @@ class ProductController {
             const productRef = await firestoreDB.collection('products').add(productData);
 
             res.status(201).json({ message: 'Produto cadastrado com sucesso', id: productRef.id, product: productData });
+            return next();
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    //UPDATE METHOD
+    async update(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name } = req.params;
+            const productData = UpdateProduct.parse(req.body);
+
+            // Verifica se o produto existe no Firestore
+            const q = firestoreDB.collection('products').where('name', '==', name);
+            const querySnapshot = await q.get();
+            if (querySnapshot.empty) {
+                return res.status(404).json({ message: 'Produto não encontrado' });
+            }
+            const productDoc = querySnapshot.docs[0];
+
+            // Atualiza o produto no Firestore
+            await productDoc.ref.update(productData);
+
+            res.status(200).json({ message: 'Produto atualizado com sucesso', product: { ...productDoc.data(), ...productData } });
             return next();
         } catch (error) {
             return next(error);
