9922ff503bb6ac8d2d92050ca63beeb95e31fc83
nrc2 <nathyre2002@gmail.com>
2024-06-08 18:16:03 -0300

    feat(ProductController): implement deactivate method

--- server/src/controllers/ProductController.ts
+++ server/src/controllers/ProductController.ts
@@ -49,6 +49,29 @@ class ProductController {
             return next(error);
         }
     }
+
+    async deactivate(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name } = req.params;
+
+            // Verifica se o produto existe no Firestore
+            const q = firestoreDB.collection('products').where('name', '==', name);
+            const querySnapshot = await q.get();
+            if (querySnapshot.empty) {
+                return res.status(404).json({ message: 'Produto não encontrado' });
+            }
+            const productDoc = querySnapshot.docs[0];
+
+            // Atualiza o status do produto para 'Indisponível'
+            await productDoc.ref.update({ status: 'Indisponível' });
+
+            res.status(200).json({ message: 'Produto desativado com sucesso', product: { ...productDoc.data(), status: 'Indisponível' } });
+            return next();
+        } catch (error) {
+            return next(error);
+        }
+    }
+
 }
 
 const productController = new ProductController();
