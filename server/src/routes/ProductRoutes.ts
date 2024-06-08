f4e753a9debdb7ca653fc79b31f8f65c15aa03b5
nrc2 <nathyre2002@gmail.com>
2024-06-08 18:01:05 -0300

    "feat(ProductRoutes.ts): implement update product route"

--- server/src/routes/ProductRoutes.ts
+++ server/src/routes/ProductRoutes.ts
@@ -4,7 +4,9 @@ import { ProductController } from '../controllers';
 const ProductRouter = Router();
 
 ProductRouter.route('/')
-    .post(ProductController.create);
+    .post(ProductController.create);//Rota para criar um novo produto
 
+ProductRouter.route('/:name')
+    .put(ProductController.update);//Rota para atualizar um produto
 
 export default ProductRouter;
