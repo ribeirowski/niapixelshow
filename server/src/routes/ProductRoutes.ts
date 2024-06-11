9a93d13e1de4a85e74dd2748305fdef9ca6f0548
nrc2 <nathyre2002@gmail.com>
2024-06-11 16:15:02 -0300

    feat(ProductRoutes.ts): implement readall product route

--- server/src/routes/ProductRoutes.ts
+++ server/src/routes/ProductRoutes.ts
@@ -4,7 +4,8 @@ import { ProductController } from '../controllers';
 const ProductRouter = Router();
 
 ProductRouter.route('/')
-    .post(ProductController.create);//Rota para criar um novo produto
+    .post(ProductController.create)//Rota para criar um novo produto
+    .get(ProductController.readAll);//Rota para pegar todos os produtos cadastrados
 
 ProductRouter.route('/:name')
     .put(ProductController.update)//Rota para atualizar um produto
