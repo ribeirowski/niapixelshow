da00bce98da61a1c598e0237fa455dbd9db305c7
nrc2 <nathyre2002@gmail.com>
2024-06-11 16:17:46 -0300

    feat(ProductController): implement read method

--- server/src/controllers/ProductController.ts
+++ server/src/controllers/ProductController.ts
@@ -110,6 +110,26 @@ class ProductController {
         }
     }
 
+    //READ METHOD
+    async read(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name } = req.params;
+
+            // Verifica se o produto existe no Firestore baseado no nome
+            const q = firestoreDB.collection('products').where('name', '==', name);
+            const querySnapshot = await q.get();
+            if (querySnapshot.empty) {
+                return res.status(404).json({ message: 'Produto não encontrado' });
+            }
+            const productDoc = querySnapshot.docs[0];
+
+            res.status(200).json({ id: productDoc.id, ...productDoc.data() });
+            return next();
+        } catch (error) {
+            return next(error);
+        }
+    }
+
 }
 
 const productController = new ProductController();
