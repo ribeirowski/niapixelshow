201530c4e96832e8187cc32a264eb737a2069d43
Victória <victoriaxavierq@gmail.com>
2024-06-15 17:03:26 -0300

    feat(promotion): fix routes

--- server/src/routes/PromotionRoutes.ts
+++ server/src/routes/PromotionRoutes.ts
@@ -3,13 +3,13 @@ import { PromotionController } from '../controllers';
 
 const PromotionRouter = Router();
 
-PromotionRouter.route('/promotion')
+PromotionRouter.route('/')
     .post(PromotionController.create); // Rota para criar uma promoção
 
-PromotionRouter.route('/promotion/all')
+PromotionRouter.route('/all')
     .get(PromotionController.readAll); // Rota para buscar todas as promoções
 
-PromotionRouter.route('/promotion/:id')
+PromotionRouter.route('/:id')
     .get(PromotionController.readById) // Rota para buscar uma promoção por ID
     .patch(PromotionController.update) // Rota para atualizar uma promoção por ID
     .delete(PromotionController.delete) // Rota para deletar uma promoção por ID
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import UserRouter from './UserRoutes';
+import PromotionRouter from './PromotionRoutes';
 
 const router = Router();
 
 router.use('/user', UserRouter);
+router.use('/promotion', PromotionRouter);
 
 router.route('/').get((_, res) => {
     res.status(200).send('Made with ❤️ by @ribeirowski');
