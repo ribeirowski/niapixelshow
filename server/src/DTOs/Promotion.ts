cd4b8c0f802bd95fe56726db136ff67f516acda7
Victória <victoriaxavierq@gmail.com>
2024-06-15 16:16:53 -0300

    feat(promotion): add promotion DTO

---
+++ server/src/DTOs/Promotion.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const Promotion = z.object({
+    name: z.string({ message: 'O nome não pode ser vazio' }),
+    discount: z
+    .number({ message: 'O desconto deve ser um número'})
+    .int({ message: 'O desconto deve ser um número de 1 até 100'})
+    .min(1, { message: 'O desconto deve ser um número de 1 até 100'})
+    .max(100, { message: 'O desconto deve ser um número de 1 até 100'}),
+    start_date: z.string({ message: 'A data de início não pode ser vazia'}),
+    end_date: z.string({ message: 'A data de término não pode ser vazia'}),
+    active: z.boolean().default(false),
+    product_id: z.string({ message: 'O id do produto não pode ser vazio' }),
+    })
