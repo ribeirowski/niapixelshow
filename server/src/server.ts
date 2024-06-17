d29af56bf42268abae7e1a7478fba0cd33da019f
Victória <victoriaxavierq@gmail.com>
2024-06-17 16:17:54 -0300

    change server port to 3001

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,5 +3,5 @@ import 'dotenv/config';
 import app from './app';
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT}`);
+    console.log(`🚀 Server ready at http://localhost:3001`);
 });
