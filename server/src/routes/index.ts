5a252dcc4ce0408ad7fc95407847cef1bbb044be
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-06-17 10:20:18 -0300

    feat: update user feature and created user logins

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import UserRouter from './UserRoutes';
+import AuthRouter from './AuthRoutes';
 
 const router = Router();
 
 router.use('/user', UserRouter);
+router.use('/login', AuthRouter);
 
 router.route('/').get((_, res) => {
     res.status(200).send('Made with ❤️ by @ribeirowski');
