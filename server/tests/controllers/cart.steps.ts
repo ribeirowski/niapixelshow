7f4554ca237136f75a2d68f4d27a5601a4c8ec1c
paivalucasss <lucaspaiva2805@gmail.com>
2024-06-18 13:25:40 -0300

    Fix: export issuo on cart test

--- server/tests/controllers/cart.steps.ts
+++ server/tests/controllers/cart.steps.ts
@@ -2,7 +2,7 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import supertest from 'supertest';
 import app from '../../src/app';
 import expect from 'expect'
-import { firestoreDB } from '../../src/services/firebaseAdmin';
+import { firestoreDBTest } from '../services/firebaseAdmin';
 import { Stats } from 'fs';
 
 const feature = loadFeature('tests/features/cart.feature');
