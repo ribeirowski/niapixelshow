3b5cf74f9a07352f99a166664ce0b5f9307c1225
paivalucasss <lucaspaiva2805@gmail.com>
2024-06-18 00:44:37 -0300

    Fix: Cart controller import error

--- server/src/controllers/CartController.ts
+++ server/src/controllers/CartController.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { firestoreDB } from '../services/firebaseAdmin';
-import { Cart, CartItem } from '@DTOs';
+import { Cart, CartItem, UpdateCartItem } from '../DTOs';
 import { hash } from 'bcryptjs';
-import { UpdateCartItem } from 'src/DTOs/Cart';
 
 class CartController {
     async get(req: Request, res: Response, next: NextFunction) {
