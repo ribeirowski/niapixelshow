7f5ebafc352f4a29f0eec61afa5e3aa2e59622e0
Victória <victoriaxavierq@gmail.com>
2024-07-04 13:19:47 -0300

    feat(promotion): controller now updates the product price

--- server/src/controllers/PromotionController.ts
+++ server/src/controllers/PromotionController.ts
@@ -1,10 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { adminAuth, firestoreDB } from '../services/firebase/firebaseAdmin';
-import { auth } from '../services/firebase/firebase';
+import { firestoreDB } from '../services/firebase/firebaseAdmin';
 import { Promotion, UpdatePromotion } from '../DTOs';
-import { errorHandler } from 'src/middlewares';
 
 class PromotionController {
+
     async create(req: Request, res: Response, next: NextFunction) {
         try {
             const promotionData = Promotion.parse(req.body);
@@ -15,10 +14,44 @@ class PromotionController {
                 return res.status(400).json({ message: 'Product not found' });
             }
 
+            // Verifica se a promoção já existe
+            const promotionQuery = await firestoreDB.collection('promotions').where('name', '==', promotionData.name).get();
+            if (!promotionQuery.empty) {
+                return res.status(400).json({ message: 'Promotion already exists' });
+            }
+
+            // Verifica se a data de início é anterior à data de término
+            if (new Date(promotionData.start_date) > new Date(promotionData.end_date)) {
+                return res.status(400).json({ message: 'Start date must be before end date' });
+            }
+
+            // Verifica se a porcentagem de desconto é um número positivo entre 0 e 100
+            if (promotionData.discount <= 0 && promotionData.discount > 100) {
+                return res.status(400).json({ message: 'Discount must be a value between 1 and 100' });
+            }            
+
             // Salva a promoção no Firestore
             await firestoreDB.collection('promotions').add(promotionData);
 
-            res.status(201).json({ message: 'Promotion created successfully' });
+            // id promoção
+            const promotionDoc = await firestoreDB.collection('promotions').where('name', '==', promotionData.name).get();
+            const id = promotionDoc.docs[0].id;
+
+            // Atualiza o produto com a referência da promoção
+            await firestoreDB.collection('products').doc(promotionData.product_id).update({ promotionId: id });
+            
+            // Pegando o preço atual do produto
+            // @ts-ignore
+            const price = productDoc.data().price;
+
+            // Calculando o novo preço com desconto
+            const newPrice = price - (price * promotionData.discount / 100);
+
+            // Atualizando o preço do produto no Firestore
+            await firestoreDB.collection('products').doc(promotionData.product_id).update({ price: newPrice });
+            
+        
+            res.status(201).json({ message: 'Promotion created successfully', id: id });
             return next();
         } catch (error) {
             return next(error);
@@ -92,8 +125,24 @@ class PromotionController {
                 return res.status(400).json({ message: 'Promotion not found' });
             }
 
+            //Id do produto
+            // @ts-ignore
+            const productId = promotionDoc.data().product_id;
+            
             // Deleta a promoção no Firestore
-            await firestoreDB.collection('promotions').doc(promotionId).delete();
+            await firestoreDB.collection('promotions').doc(promotionId).delete()
+            
+            // Atualiza o produto com a referência da promoção
+            await firestoreDB.collection('products').doc(productId).update({ promotionId: null });
+
+            //Atualizando o preço do produto
+            const productDoc = await firestoreDB.collection('products').doc(productId).get();
+            // @ts-ignore
+            const price = productDoc.data().price;
+            // @ts-ignore
+            const discount = promotionDoc.data().discount;
+            const newPrice = price / (1 - (discount / 100));
+            await firestoreDB.collection('products').doc(productId).update({ price: newPrice });
 
             res.status(200).json({ message: 'Promotion deleted successfully' });
             return next();
