65841917ea62d833d8ecb189fa718e094d9f6a84
Filipe Maciel <filipemleicht@gmail.com>
2024-07-20 22:22:57 -0300

    added date filter to stats

--- server/tests/controllers/stats.steps.ts
+++ server/tests/controllers/stats.steps.ts
@@ -1,23 +1,33 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import supertest from 'supertest';
 import app from '../../src/app';
-import { firestoreDB } from '../../src/services/firebase/firebaseAdmin';
+import { firestoreDB, adminAuth } from '../../src/services/firebase/firebaseAdmin';
 import { Stats } from 'fs';
 import { expect } from 'expect';
 
 const feature = loadFeature('tests/features/stats.feature');
 
+const usedEmail = 'thiagojgcosta@gmail.com';
+
 defineFeature(feature, (test)=>{
     let request = supertest(app)
     let response: supertest.Response;
+    jest.setTimeout(15000);
+
+    afterEach(async () => {
+        const order = await firestoreDB.collection('orders').where('email', '==', usedEmail).get();
+        const user = await firestoreDB.collection('users').where('email', '==', usedEmail).get();
+        const batch = firestoreDB.batch();
+        order.forEach(doc => batch.delete(doc.ref));
+        user.forEach(doc => batch.delete(doc.ref));
+        await batch.commit();
+        const deletePromises = user.docs.map(async doc => await adminAuth.deleteUser(doc.id));
+        await Promise.all(deletePromises);
+    });
 
     // Teste 1
     test('Requisição de Estatísticas', ({given, when, then}) => {
         given(/^o banco de dados tem pedido com qtd "(.*)", date "(.*)", item "(.*)", price "(.*)", description "(.*)", addr "(.*)", email "(.*)" e status "(.*)"$/, async (qtd, date, item, price, description, addr, email, status) => {
-            const orders = await firestoreDB.collection('orders').get();
-            const batch = firestoreDB.batch();
-            orders.forEach(doc => batch.delete(doc.ref));
-            await batch.commit();
             const orderData = {
                 email: email,
                 item: item,
@@ -31,28 +41,31 @@ defineFeature(feature, (test)=>{
             await request.post('/order').send(orderData);
         });
         when(/^são requisitadas as estatísticas de vendas$/, async () => {
-            response = await request.get('/order/stats');
+            response = await request.get('/order/stats?year=0000&month=06');
         });
         then(/^o sistema retorna vendas totais "(.*)", produto mais vendido "(.*)" e tabela de produtos com produto "(.*)" com quantidade "(.*)" e valor total "(.*)"$/, async (totalValue, mostSold, productName, productAmount, productValue) => {
-            const expected = {"mostSold": mostSold, "productAmount": [parseInt(productAmount)], "productName": [productName], "productValue": [parseFloat(productValue)], "totalValue": parseFloat(totalValue)};
-            expect(response.body).toStrictEqual(expected);
+            expect(response.body.totalValue).toBe(parseFloat(totalValue));
+            expect(response.body.mostSold).toBe(mostSold);
+            expect(response.body.productName[0]).toBe(productName);
+            expect(response.body.productAmount[0]).toBe(parseInt(productAmount));
+            expect(response.body.productValue[0]).toBe(parseFloat(productValue));
         });
     });
 
     // Teste 2
     test('Requisição sem Pedidos Pagos', ({given, when, then}) => {
         given(/^o banco de dados não tem pedidos$/, async () => {
-            const orders = await firestoreDB.collection('orders').get();
-            const batch = firestoreDB.batch();
-            orders.forEach(doc => batch.delete(doc.ref));
-            await batch.commit();
+            
         });
         when(/^são requisitadas as estatísticas de vendas$/, async () => {
             response = await request.get('/order/stats');
         });
         then(/^o sistema retorna vendas totais "(.*)", produto mais vendido "(.*)" e tabela de produtos com produto "(.*)" com quantidade "(.*)" e valor total "(.*)"$/, async (totalValue, mostSold, productName, productAmount, productValue) => {
-            const expected = {"mostSold": mostSold, "productAmount": [parseInt(productAmount)], "productName": [productName], "productValue": [parseFloat(productValue)], "totalValue": parseFloat(totalValue)};
-            expect(response.body).toStrictEqual(expected);
+            expect(response.body.totalValue).toBe(parseFloat(totalValue));
+            expect(response.body.mostSold).toBe(mostSold);
+            expect(response.body.productName[0]).toBe(productName);
+            expect(response.body.productAmount[0]).toBe(parseInt(productAmount));
+            expect(response.body.productValue[0]).toBe(parseFloat(productValue));
         });
     });
 })
