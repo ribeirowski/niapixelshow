8153469530bb2dcbc291cf1a9de0dc08246d8356
nrc2 <nathyre2002@gmail.com>
2024-07-22 01:27:17 -0300

    feat: call create function

--- client/src/pages/product/create.tsx
+++ client/src/pages/product/create.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Link from 'next/link';
 import { Box, Typography, Container, Button } from '@mui/material';
 import ProductForm from '@/components/ProductForm';
+import {useProduct} from '@/hooks'
+import { useRouter } from 'next/router';
 
 const CreateProductPage: React.FC = () => {
+  const router = useRouter()
+  const {createProduct} = useProduct();
+
   const handleSubmit = (data: any) => {
-    console.log('Produto cadastrado:', data);
-    // Adicione a lógica para salvar o produto
+    createProduct(data).then(()=>{router.push(`/product`);})
   };
 
   return (
