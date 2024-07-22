166747c8bfc726e960942caeed813745f2ea9366
Thiago Costa <thiagojgcosta@gmail.com>
2024-07-21 22:31:42 -0300

    feat: When changed the order status now is sent an email to the user, and added an Order Type

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,6 +2,7 @@ import { Product, Category } from "./ProductTypes";
 import { userSchema, UserSchema, UseUserReturn, UpdateUser } from "./UserTypes";
 import { forgotPasswordSchema } from "./ForgotPasswordTypes";
 import { loginSchema } from "./AuthTypes";
+import { Order, UseOrderInterface } from "./OrderTypes";
 
 export { Product, Category, userSchema, forgotPasswordSchema, loginSchema, UpdateUser };
-export type { UserSchema, UseUserReturn };
+export type { UserSchema, UseUserReturn, Order, UseOrderInterface };
