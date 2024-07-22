5708a6551d19a55cf9fa0265dee7ce0050c31ce9
nrc2 <nathyre2002@gmail.com>
2024-07-22 15:08:31 -0300

    feat: add a message pop up in the create function

--- client/src/pages/product/create.tsx
+++ client/src/pages/product/create.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Container, Snackbar, Alert } from '@mui/material';
 import ProductForm from '@/components/ProductForm';
 import { useProduct } from '@/hooks';
 import { useRouter } from 'next/router';
@@ -9,11 +9,24 @@ const CreateProductPage: React.FC = () => {
   const router = useRouter(); // Hook do Next.js para roteamento
   const { createProduct } = useProduct(); // Hook personalizado para criar um produto
 
+  // Estados para controlar o Snackbar
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  // Função para lidar com o fechamento do Snackbar
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
   // Função de callback para lidar com o envio do formulário
   const handleSubmit = (data: any) => {
     // Chama a função createProduct e, em seguida, redireciona para a página de produtos
     createProduct(data).then(() => {
-      router.push(`/product`);
+      setSnackbarMessage('Produto cadastrado com sucesso!');
+      setSnackbarOpen(true);
+      setTimeout(() => {
+        router.push(`/product`);
+      }, 2000);
     });
   };
 
@@ -23,6 +36,12 @@ const CreateProductPage: React.FC = () => {
         {/* Componente ProductForm que renderiza o formulário para criar um produto */}
         <ProductForm onSubmit={handleSubmit} />
       </Box>
+      {/* Snackbar para exibir mensagens de sucesso */}
+      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
+        <Alert onClose={handleSnackbarClose} severity="success" sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
