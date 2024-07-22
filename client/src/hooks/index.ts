73bc602d8346c9556f55d44bacda3042a024596f
Victória <victoriaxavierq@gmail.com>
2024-07-22 14:03:40 -0300

    feat(promotion): update hooks with new exports

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -2,5 +2,6 @@ import useUser from "./useUser";
 import useAuth, { AuthProvider } from "./useAuth";
 import useProduct from "./useProduct";
 import useOrder from "./useOrder";
+import usePromotion from "./usePromotion";
 
-export { useUser, useAuth, AuthProvider, useProduct, useOrder };
+export { useUser, useAuth, AuthProvider, useProduct, useOrder, usePromotion };
