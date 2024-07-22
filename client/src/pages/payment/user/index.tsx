43b003a1705aa0fbccc26215de7d3e45e32abdec
Thiago Costa <thiagojgcosta@gmail.com>
2024-07-22 10:18:32 -0300

    feat: add qrCode and códiggo copia e cola for payment, and delete cart after orders are done

--- client/src/pages/payment/user/index.tsx
+++ client/src/pages/payment/user/index.tsx
@@ -8,7 +8,7 @@ const PaymentPage = () => {
     const router = useRouter();
     const { loading, error } = useOrder();
     const { users } = useUser();
-    const [paymentCode] = useState<string>('COPIA_E_COLA_CODE'); // Substitua pelo código real
+    const [paymentCode] = useState<string>('00020101021126430014br.gov.bcb.pix0121nathyre2002@gmail.com5204000053039865802BR5923NATHALIA REZENDE COELHO6007ARACAJU62070503***6304A3A7'); // Substitua pelo código real
 
     const handleBack = () => {
         router.back();
@@ -51,7 +51,7 @@ const PaymentPage = () => {
                     QR Code
                 </Typography>
                 <Box sx={{ textAlign: 'center' }}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/nia-pixel-show.appspot.com/o/shereko.png?alt=media&token=e707c37c-822a-40ea-8252-e185312a30da" alt="QR Code para Pagamento" style={{ maxWidth: '100%' }} />
+                    <img src="https://firebasestorage.googleapis.com/v0/b/nia-pixel-show.appspot.com/o/qrCode.jpg?alt=media&token=b7e43210-5afc-4fe6-9c7e-9a3391fed8cd" alt="QR Code para Pagamento" style={{ maxWidth: '100%' }} />
                 </Box>
             </Paper>
             <Box textAlign='center'>
