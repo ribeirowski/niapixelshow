fbc41c042d4c84de892a8c66de02bd67af0a98d0
nrc2 <nathyre2002@gmail.com>
2024-07-22 00:36:54 -0300

    fix: adapt hooks to back responses

--- client/src/hooks/useProduct.ts
+++ client/src/hooks/useProduct.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import api from '@/services/api';
 import { Product, Category } from '@/types';	
 
-interface Product {
+export interface Product {
     image?: string;
     id?: string;
     name: string;
@@ -35,12 +35,17 @@ const useProduct = (): UseProductsInterface<Product> => {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
 
-    const handleApiCall = async <T,>(apiCall: Promise<{ data: T }>): Promise<T> => {
+    const handleApiCall = async <T,>(apiCall: Promise<{ data: { product: T } | T }>): Promise<T> => {
         setLoading(true);
         setError(null);
         try {
             const response = await apiCall;
-            return response.data;
+            const data = response.data;
+            if (data && typeof data === 'object' && 'product' in data) {
+                return data.product;
+            } else {
+                return data as T;
+            }
         } catch (err: any) {
             const errorMessage = err.response?.data.message || 'Ocorreu um erro inesperado';
             setError(errorMessage);
@@ -59,8 +64,8 @@ const useProduct = (): UseProductsInterface<Product> => {
     };
 
     const getProductById = useCallback(async (productId: string) => {
-        const response = await handleApiCall<{ data: Product }>(api.get(`/product/${productId}`));
-        setProductData(response);
+        const response = await handleApiCall<Product>(api.get(`/product/${productId}`));
+        setProductData(response)
     }, []);
 
     const deleteProduct = async (productId: string) => {
@@ -68,7 +73,7 @@ const useProduct = (): UseProductsInterface<Product> => {
     };
 
     const getAllProducts = useCallback(async () => {
-        const response = await handleApiCall<{ data: Product[] }>(api.get('/product/'));
+        const response = await handleApiCall< Product[] >(api.get('/product/'));
         setProducts(response);
     }, []);
 
