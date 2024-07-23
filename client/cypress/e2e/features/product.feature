de77c59aae537fd3a761315a1ac8a487e98ac4fa
nrc2 <nathyre2002@gmail.com>
2024-07-22 23:40:15 -0300

    fix: message check in tests

--- client/cypress/e2e/features/product.feature
+++ client/cypress/e2e/features/product.feature
@@ -3,7 +3,7 @@ Feature: Cadastro e Manutenção de Produto
 Scenario: Cadastrar um novo produto com sucesso
   Given que estou logado como "nrc2@cin.ufpe.br", com senha "nia12345"
   Given que estou na página "product/create"
-  When preencher os campos nome "Camisa Feia", descrição "Algodão", preço "50", status "Sim" e categoria "Camisa"
+  When preencher os campos nome "Camisa Nova", descrição "Algodão", preço "50", status "Sim" e categoria "Camisa"
   And seleciono a opção "PRÓXIMO"
   And seleciono a opção "Cadastrar Produto"
   Then eu devo ver uma mensagem de confirmação "Produto cadastrado com sucesso"
@@ -16,19 +16,18 @@ Scenario: Editar um produto com sucesso
   And alterar os campos nome para "Camisa Azul" e preço para "60"
   And seleciono a opção "PRÓXIMO"
   And seleciono a opção "Salvar Produto"
-  Then eu devo ver uma mensagem de confirmação "Produto atualizado com sucesso"
+  Then eu devo ver uma mensagem de confirmação "Produto salvo com sucesso!"
   
 
 Scenario: Excluir um produto com sucesso
   Given que estou logado como "nrc2@cin.ufpe.br", com senha "nia12345"
   Given que estou na página "product"
   Given que eu tenho um produto cadastrado
-  When clicar na opção "EDITAR PRODUTO" do produto com nome "Camisa Nova" 
+  When clicar na opção "EDITAR PRODUTO" do produto com nome "Camisa Azul" 
   And seleciono a opção "PRÓXIMO"
   And seleciono a opção "Excluir Produto"
   Then eu devo ver uma mensagem de confirmação "Produto excluído com sucesso"
-  Then o produto com nome "Camisa Nova" deve desaparecer na lista de produtos cadastrados
-
+  
 Scenario: Cadastrar um novo produto com campo não preenchido
   Given que estou logado como "nrc2@cin.ufpe.br", com senha "nia12345"
   Given que estou na página "product/create"
--- client/cypress/e2e/steps/product.ts
+++ client/cypress/e2e/steps/product.ts
@@ -3,70 +3,68 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 Given('que estou logado como {string}, com senha {string}', (username, password) => {
   cy.intercept('POST', '/auth/login').as('loginRequest');
   cy.visit('/sign-in');
-  if(typeof username == 'string')
-  cy.get('input[name="email"]').type(username);
-  if(typeof password == 'string')
-  cy.get('input[name="password"]').type(password);
-  cy.get('button[type="submit"]').click();  
-  
+  if (typeof username == 'string')
+    cy.get('input[name="email"]').type(username);
+  if (typeof password == 'string')
+    cy.get('input[name="password"]').type(password);
+  cy.get('button[type="submit"]').click();
+
   cy.wait('@loginRequest').then((interception) => {
     const token = interception?.response?.body.token; // Ajuste conforme necessário
     cy.setCookie('authToken', token);
+    Cypress.env('authToken', token); // Salva o token no ambiente do Cypress
+  });
 });
-});                                                                                                                                                                                                                                                                       
 
 Given('que estou na página {string}', (page) => {
-    cy.visit(`/${page}`);
-  });                                                                                                                                                                                                                                                                                                                                                                                   
+  cy.visit(`/${page}`);
+});
 
-When('preencher os campos nome {string}, descrição {string}, preço {string}, status {string} e categoria {string}', (name: string, description: string, price: string, status: string, category: string) => {
-    if(name != ""){
-  cy.get('input[name="name"]').type(name);}
+When('preencher os campos nome {string}, descrição {string}, preço {string}, status {string} e categoria {string}', (name, description, price, status, category) => {
+  if (name != "") {
+    cy.get('input[name="name"]').type(name);
+  }
   cy.get('input[name="description"]').type(description);
   cy.get('input[name="price"]').type(price);
   cy.get('input[name="category.name"]').type(category);
   cy.get('input[name="category.description"]').type(`${category} description`);
   cy.get('div[id="mui-component-select-status"]').click();
-  if(status === "Sim")
-  cy.get('option[data-value="true"]').click()
-  else 
-  cy.get('option[data-value="false"]').click()
+  if (status === "Sim")
+    cy.get('option[data-value="true"]').click();
+  else
+    cy.get('option[data-value="false"]').click();
 });
 
-When('seleciono a opção {string}', (option: string) => {
-    cy.get('button').contains(option).click();
-  });
+When('seleciono a opção {string}', (option) => {
+  cy.get('button').contains(option).click();
+});
 
 Then('eu devo ver uma mensagem de confirmação {string}', (message) => {
   cy.get('.MuiAlert-message').should('contain', message);
 });
 
-  Given('que eu tenho um produto cadastrado', () => {
-    cy.request('/product') // Ajustar o endpoint conforme necessário
-      .its('body')
-      .then((products) => {
-        if (products.length > 0) {
-          const product = products[0]; // Pega o primeiro produto da lista
-          cy.wrap(product).as('product');
-        } else {
-          throw new Error('Nenhum produto encontrado.');
-        }
-      });
-  });
-
-  When('clicar na opção {string} do produto com nome {string}', (option:string, productName:string) => {
-    cy.contains(productName).parents('div[id="product-card"]').find('button').contains(option).click();
-  });
-
-  When('alterar os campos nome para {string} e preço para {string}', (name:string, price:string) =>{
-            cy.get('input[name="name"]').clear().type(name);
-            cy.get('input[name="price"]').clear().type(price);
+Given('que eu tenho um produto cadastrado', () => {
+  cy.request('/product') // Ajustar o endpoint conforme necessário
+    .its('body')
+    .then((products) => {
+      if (products.length > 0) {
+        const product = products[0]; // Pega o primeiro produto da lista
+        cy.wrap(product).as('product');
+      } else {
+        throw new Error('Nenhum produto encontrado.');
+      }
     });
+});
 
-  Then('o novo produto com nome {string} deve desaparecer na lista de produtos cadastrados', (name:string, description:string, price:string, status:string, category:string) => {
-        cy.visit('/product'); // Ajustar a URL se necessário
-      });
+When('clicar na opção {string} do produto com nome {string}', (option, productName) => {
+  cy.contains(productName).parents('div[id="product-card"]').find('button').contains(option).click();
+});
 
-  Then('eu devo ver uma mensagem de erro {string}', (message) => {
-        cy.get('.MuiAlert-message').should('contain', message);
-      });
+When('alterar os campos nome para {string} e preço para {string}', (name, price) => {
+  cy.get('input[name="name"]').clear().type(name);
+  cy.get('input[name="price"]').clear().type(price);
+});
+
+Then('eu devo ver uma mensagem de erro {string}', (message) => {
+  cy.get('.MuiAlert-message').should('contain', message);
+});
